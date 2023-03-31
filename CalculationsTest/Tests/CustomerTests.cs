3d9ffdd044b5d2caa844781be51b6d621cd19a12
Aslan17 <k.aslan1707@gmail.com>
2023-03-31 17:39:20 +0400

    Customer Tests Added

---
+++ CalculationsTest/Tests/CustomerTests.cs
@@ -0,0 +1,26 @@
+﻿using UnitTesting.Models;
+using Xunit;
+
+namespace CalculationsTest.Tests
+{
+	public class CustomerTests
+	{
+		[Fact]
+		public void GetOrdersCountByName_NotNull()
+		{
+			var customer = new Customer();
+			var exceptionDetails = Assert.Throws<ArgumentException>(() => customer.GetOrdersCountByName(null!));
+			Assert.Equal("Name can't be null or empty", exceptionDetails.Message);
+		}
+
+		[Fact]
+		public void LoyalCustomerForOrdersG100()
+		{
+			const int LoyalCustomerDiscount = 10;
+			var customer = CustomerFactory.CreateCustomer(101);
+			var actual = Assert.IsType<LoyalCustomer>(customer);
+			Assert.Equal(LoyalCustomerDiscount, actual.Discount);
+		}
+
+	}
+}
---
+++ UnitTesting/Models/Customer.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace UnitTesting.Models
+{
+	public class Customer
+	{
+		public string Name { get; set; } = String.Empty;
+
+		public int GetOrdersCountByName(string name)
+		{
+			if (String.IsNullOrEmpty(name)) throw new ArgumentException("Name can't be null or empty");
+			return 100;
+		}
+	}
+
+	public class LoyalCustomer : Customer
+	{
+		public int Discount { get; set; } = 10;
+	}
+
+	public static class CustomerFactory
+	{
+		public static Customer CreateCustomer(int orderCount) 
+		{
+			return orderCount <= 100 ? new Customer() : new LoyalCustomer(); 
+		}
+	}
+}
